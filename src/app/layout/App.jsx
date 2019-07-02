9655df0d3144eb25ab4771f3e2071dc269e8c0d3
Linh <tuanlinh18@gmail.com>
2019-07-02 22:00:48 +0700

    setting initial project

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar'
+import '../../index.css'
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
-          App          
-      </div>
+      <Fragment>
+        <NavBar/>
+        <Container className="main">
+          <EventDashboard/> 
+        </Container>
+      </Fragment>
     );
   }
 }
