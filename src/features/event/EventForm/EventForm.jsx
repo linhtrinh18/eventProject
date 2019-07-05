f9d0ddcc4a9cd3808b75d2d7c87772ed9de73160
Linh <tuanlinh18@gmail.com>
2019-07-05 19:29:04 +0700

    test

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,55 +1,87 @@
-import React, { Component } from 'react'
-import { Segment, Form, Button } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Segment, Form, Button } from "semantic-ui-react";
 
 class EventForm extends Component {
-	state = {
-		title: ''
-	}
-	handleFormSubmit = (event) => {
-		event.preventDefault();
-		console.log(this.state)
-	}
-	
-	handleTitleChange = (evt) => {
-		this.setState({
-			title: evt.target.value
-		})
-	}
-	
-    render() {
-      const {cancelFormOpen} = this.props
-	  const {title} = this.state;
-        return (
-                  <Segment>
-                    <Form onSubmit={this.handleFormSubmit}>
-                      <Form.Field>
-                        <label>Event Title</label>
-                        <input onChange={this.handleTitleChange} value={title} placeholder="Event Title" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>Event Date</label>
-                        <input type="date" placeholder="Event Date" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>City</label>
-                        <input placeholder="City event is taking place" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>Venue</label>
-                        <input placeholder="Enter the Venue of the event" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>Hosted By</label>
-                        <input placeholder="Enter the name of person hosting" />
-                      </Form.Field>
-                      <Button positive type="submit">
-                        Submit
-                      </Button>
-                      <Button type="button" onClick={cancelFormOpen}>Cancel</Button>
-                    </Form>
-                  </Segment>
-        )
-    }
+  state = {
+    title: "",
+    date: "",
+    city: "",
+    venue: "",
+    hostedBy: ""
+  };
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  handleTitleChange = evt => {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  };
+
+  render() {
+    const { cancelFormOpen } = this.props;
+    const { title, date, city, venue, hostedBy } = this.state;
+    return (
+      <Segment>
+        <Form onSubmit={this.handleFormSubmit} autoComplete='off'>
+          <Form.Field>
+            <label>Event Title</label>
+            <input
+              name="title"
+              onChange={this.handleTitleChange}
+              value={title}
+              placeholder="Event Title"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Event Date</label>
+            <input
+              type="date"
+              name="date"
+              onChange={this.handleTitleChange}
+              value={date}
+              placeholder="Event Date"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>City</label>
+            <input
+              name="city"
+              onChange={this.handleTitleChange}
+              value={city}
+              placeholder="City event is taking place"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Venue</label>
+            <input
+              name="venue"
+              onChange={this.handleTitleChange}
+              value={venue}
+              placeholder="Enter the Venue of the event"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Hosted By</label>
+            <input
+              name="hostedBy"
+              onChange={this.handleTitleChange}
+              value={hostedBy}
+              placeholder="Enter the name of person hosting"
+            />
+          </Form.Field>
+          <Button positive type="submit">
+            Submit
+          </Button>
+          <Button type="button" onClick={cancelFormOpen}>
+            Cancel
+          </Button>
+        </Form>
+      </Segment>
+    );
+  }
 }
 
-export default EventForm
+export default EventForm;
